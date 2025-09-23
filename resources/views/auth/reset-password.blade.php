f478745cc04aef84afebb2d5822591c095b6a818
starman <msaadch1996@gmail.com>
2025-09-24 00:09:44 +0500

    changes

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -50,84 +50,91 @@
             <x-alert type="error" :message="$errors->first()" />
         @endforeach
     @endif
-    <section class="section-content-center py-4">
-        <div class="text-left mb-3">
-            
-            <h2 class="text-2xl font-bold mt-4 mb-1 let_spc">Update Password</h2>
-            <p class="text-gray-500 text-base">
-                Enter a new password to secure your account. Once updated, you’ll be logged out and will need to log in again with your new password.
-            </p>
-        </div>
-        <form method="POST" action="{{ route('password.store') }}">
-            @csrf
-            <div class="card bg-white mb-3">
-                <!-- Password Reset Token -->
-                <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-                <input type="hidden" name="email" value="{{ $request->email }}">
-                <div x-data="{ password: '', confirm: '' }">
-                    <!-- Password -->
-                    <div class="">
-                        <label for="password" class="input-label mb-1 fw-sm">
-                            New Password
-                            <span class="tooltip-icon " data-bs-toggle="tooltip" title="Add new password">
-                                <i class="fa fa-question-circle hover-blue"></i>
-                            </span>
-                        </label>
-                        <x-text-input 
-                            id="password" 
-                            class="block mt-1 w-full" 
-                            type="password" 
-                            name="password" 
-                            x-model="password"
-                            required 
-                            autocomplete="new-password" 
-                        />
-                        <!-- <x-input-error :messages="$errors->first('password')" class="mt-2" /> -->
-                    </div>
-
-                    <!-- Confirm Password -->
-                    <div class="mt-3">
-                        <label for="password_confirmation" class="input-label mb-1 fw-sm">
-                            Confirm New Password
-                            <span class="tooltip-icon " data-bs-toggle="tooltip" title="Confirm new password">
-                                <i class="fa fa-question-circle hover-blue"></i>
-                            </span>
-                        </label>
-                        <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                            type="password"
-                                            x-model="confirm"
-                                            name="password_confirmation" required autocomplete="new-password" />
-                        <!-- <x-input-error :messages="$errors->first('password_confirmation')" class="mt-2" /> -->
-                    </div>
-
-                    <!-- Password Requirements -->
-                    <ul class="mt-4 text-sm text-gray-500 space-y-1">
-                        <li class="flex items-center" :class="password.length >= 8 ? 'text-green-500' : 'text-gray-500'">
-                            <i class="fa-regular fa-circle-check mr-2" :class="password.length >= 8 ? 'text-green-500' : 'text-gray-500'"></i> Minimum 8 Characters
-                        </li>
-                        <li class="flex items-center" :class="/[A-Z]/.test(password) ? 'text-green-500' : 'text-gray-500'">
-                            <i class="fa-regular fa-circle-check mr-2" :class="/[A-Z]/.test(password) ? 'text-green-500' : 'text-gray-500'"></i> At least one uppercase letter
-                        </li>
-                        <li class="flex items-center" :class="/[\W_]/.test(password) ? 'text-green-500' : 'text-gray-500'">
-                            <i class="fa-regular fa-circle-check mr-2" :class="/[\W_]/.test(password) ? 'text-green-500' : 'text-gray-500'"></i> At least one special character
-                        </li>
-                        <li class="flex items-center" :class="/[0-9]/.test(password) ? 'text-green-500' : 'text-gray-500'">
-                            <i class="fa-regular fa-circle-check mr-2" :class="/[0-9]/.test(password) ? 'text-green-500' : 'text-gray-500'"></i> At least one number
-                        </li>
-                        <li class="flex items-center" :class="password && password === confirm ? 'text-green-500' : 'text-gray-500'">
-                            <i class="fa-regular fa-circle-check mr-2" :class="password && password === confirm ? 'text-green-500' : 'text-gray-500'"></i>
-                            Passwords match
-                        </li>
-                    </ul>
-                </div>
-                <div class="flex items-center mt-3">
-                    <x-primary-button class="text-sm">
-                        {{ __('Update Password') }}
-                    </x-primary-button>
-                </div>
+        <div>
+        <section class="section-content-center py-4 mb-4">
+            <div class="text-left mb-3">
+                
+                <h2 class="text-2xl font-bold mt-4 mb-1 let_spc">Update Password</h2>
+                <p class="text-gray-500 text-base">
+                    Enter a new password to secure your account. Once updated, you’ll be logged out and will need to log in again with your new password.
+                </p>
             </div>
-        </form>
-    </section> 
+            <form method="POST" action="{{ route('password.store') }}">
+                @csrf
+                <div class="card bg-white mb-3">
+                    <!-- Password Reset Token -->
+                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                    <input type="hidden" name="email" value="{{ $request->email }}">
+                    <div x-data="{ password: '', confirm: '' }">
+                        <!-- Password -->
+                        <div class="">
+                            <label for="password" class="input-label mb-1 fw-sm">
+                                New Password
+                                <span class="tooltip-icon " data-bs-toggle="tooltip" title="Add new password">
+                                    <i class="fa fa-question-circle hover-blue"></i>
+                                </span>
+                            </label>
+                            <x-text-input 
+                                id="password" 
+                                class="block mt-1 w-full" 
+                                type="password" 
+                                name="password" 
+                                x-model="password"
+                                required 
+                                autocomplete="new-password" 
+                            />
+                            <!-- <x-input-error :messages="$errors->first('password')" class="mt-2" /> -->
+                        </div>
+
+                        <!-- Confirm Password -->
+                        <div class="mt-3">
+                            <label for="password_confirmation" class="input-label mb-1 fw-sm">
+                                Confirm New Password
+                                <span class="tooltip-icon " data-bs-toggle="tooltip" title="Confirm new password">
+                                    <i class="fa fa-question-circle hover-blue"></i>
+                                </span>
+                            </label>
+                            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                                type="password"
+                                                x-model="confirm"
+                                                name="password_confirmation" required autocomplete="new-password" />
+                            <!-- <x-input-error :messages="$errors->first('password_confirmation')" class="mt-2" /> -->
+                        </div>
+
+                        <!-- Password Requirements -->
+                        <ul class="mt-4 text-sm text-gray-500 space-y-1">
+                            <li class="flex items-center" :class="password.length >= 8 ? 'text-green-500' : 'text-gray-500'">
+                                <i class="fa-regular fa-circle-check mr-2" :class="password.length >= 8 ? 'text-green-500' : 'text-gray-500'"></i> Minimum 8 Characters
+                            </li>
+                            <li class="flex items-center" :class="/[A-Z]/.test(password) ? 'text-green-500' : 'text-gray-500'">
+                                <i class="fa-regular fa-circle-check mr-2" :class="/[A-Z]/.test(password) ? 'text-green-500' : 'text-gray-500'"></i> At least one uppercase letter
+                            </li>
+                            <li class="flex items-center" :class="/[\W_]/.test(password) ? 'text-green-500' : 'text-gray-500'">
+                                <i class="fa-regular fa-circle-check mr-2" :class="/[\W_]/.test(password) ? 'text-green-500' : 'text-gray-500'"></i> At least one special character
+                            </li>
+                            <li class="flex items-center" :class="/[0-9]/.test(password) ? 'text-green-500' : 'text-gray-500'">
+                                <i class="fa-regular fa-circle-check mr-2" :class="/[0-9]/.test(password) ? 'text-green-500' : 'text-gray-500'"></i> At least one number
+                            </li>
+                            <li class="flex items-center" :class="password && password === confirm ? 'text-green-500' : 'text-gray-500'">
+                                <i class="fa-regular fa-circle-check mr-2" :class="password && password === confirm ? 'text-green-500' : 'text-gray-500'"></i>
+                                Passwords match
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="flex items-center mt-3">
+                        <x-primary-button class="text-sm">
+                            {{ __('Update Password') }}
+                        </x-primary-button>
+                    </div>
+                </div>
+            </form>
+        </section> 
+        <footer class="bg-white text-center py-2 border-t">
+            <p class="text-md font-bold text-gray-600">
+                © 2025 <span class="font-bold">InsightHQ</span>. All rights reserved.
+            </p>
+        </footer>
+    </div>
 </body>
 </html>
